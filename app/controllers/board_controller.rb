85c181514a3b4a3541269b4166f8a99492c8f17f
kwonhyunwoo <kwonhyunwoo@likelion.org>
2017-06-06 14:20:45 +0000

    realpush

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -2,13 +2,76 @@ class BoardController < ApplicationController
   def lb #강의평가 게시판
     
   end
+
+  
+  ## 자유게시판 
+  
   def cb #자유게시판
-    
+    @cbb = Cbb.all
   end
   
-  def qb #질문 게시판
+  def create  #자유게시판 db접근
+        @cbbs = Cbb.new
+        @cbbs.title = params[:input_title]
+        @cbbs.content = params[:input_content]
+        @cbbs.save
+        redirect_to '/board/cb'
   end
   
   def newcb #자유게시판 글쓰기
   end
+  
+  def cdestroy #자유게시판 삭제
+    @cbbs = Cbb.find(params[:board_id])
+    @cbbs.destroy
+    redirect_to '/board/cb'
+  end 
+  
+  def cupdate
+    @cbbs = Cbb.find(params[:board_id])
+    @cbbs.title = params[:input_title]
+    @cbbs.content = params[:input_content]
+    @cbbs.save
+    
+    redirect_to "/board/cshow/#{@cbbs.id}"
+  end  
+  def cedit
+    @cbbs = Cbb.find(params[:board_id])
+  end
+  
+  def cshow #자유게시판 각각의 show
+    @cbbs = Cbb.find(params[:board_id])
+  end 
+  
+  #질문게시판
+  def qb #질문 게시판
+    @qbb = Qbb.all
+  end
+  
+  def qcreate #Q&A db접근
+        @qbb = Qbb.new
+        @qbb.title = params[:qinput_title]
+        @qbb.content = params[:qinput_content]
+        @qbb.save
+        
+        redirect_to '/board/qb'
+  end
+
+  def newqb #Q&A 글쓰기
+  end
+  
+  
+  def qshow #Q&A 각각의 show
+  end
+  
+  
+  def qdestroy #Q&A 삭제
+  end
+  
+  def qedit
+  end
+
+  def qupdate
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,31 @@ Rails.application.routes.draw do
 
   get 'board/lb'
   
-  get 'board/qb'
+  get '/board/qb' => 'board#qb'
   
-  get 'board/cb' => 'board#cb'
+  get '/board/cb' => 'board#cb'
   
   get '/home/index' => 'home#index'
   
   #자유게시판 글쓰기
   get '/board/newcb' => 'board#newcb'
-
+  post '/board/create' => 'board#create'
+  
+  #Q&A 글쓰기
+  get '/board/newqb' => 'board#newqb'
+  post '/board/qcreate' => 'board#qcreate'
+  
+  #자유게시판show
+  get '/board/cshow/:board_id' => 'board#cshow'
+  #Q&A show
+  get '/board/qshow' => 'board#qshow'
+  
+  #자유게시판 삭제
+  post '/board/cdestroy/:board_id' => 'board#cdestroy'
+  
+  #자유게시판 수정
+  get '/board/cedit/:board_id' => 'board#cedit'
+  post '/board/cupdate/:board_id' => 'board#cupdate'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
