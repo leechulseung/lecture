29b74262c928a28b32f59069569115a13db6bb08
gelos <gelos@likelion.org>
2017-06-10 01:32:48 +0000

    Front 패치

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
 
   # @user.errors.messages.each do |key, value|
   #   @email_error = value[0]
